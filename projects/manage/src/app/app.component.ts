1c6ed781637dd154c8542e60f5bd4e857b4b9c18
bullsgl <bullsgl@gmail.com>
2018-10-12 08:56:33 +0800

    管理界面

--- projects/manage/src/app/app.component.ts
+++ projects/manage/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+declare var $: any; // for import jquery
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'manage';
+export class AppComponent implements AfterViewInit {
+  title = '纬创抽奖后台管理';
+
+  public ngAfterViewInit()
+  {
+    $('.menu .item').tab();
+  }
+
+  listEmp() {
+    console.log('### List Emp Info!');
+  }
+
+  listPrize() {
+    console.log('### List Prize Info!');
+  }
+
+  listWinner() {
+    console.log('### List Winner Info!');
+  }
 }
